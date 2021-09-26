5c1277e2aca7a986866ef8be3bd9a61eb91b6566
Sergey Dvoraninovich <sergey.dvoraninovich@gmail.ru>
2021-09-26 22:30:00 +0300

    initial javadoc for DishDao & BaseDao

--- src/main/java/com/dvoraninovich/establishment/model/dao/BaseDao.java
+++ src/main/java/com/dvoraninovich/establishment/model/dao/BaseDao.java
@@ -5,12 +5,44 @@ import com.dvoraninovich.establishment.exception.DaoException;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * {@code BaseDao} is a top interface in the DAO hierarchy.
+ * @param <K> the type parameter of id of stored entity
+ * @param <T> the type parameter of stored entity
+ */
 public interface BaseDao<K, T> {
+    /**
+     * Find all stored entities.
+     *
+     * @return the list of specified entities
+     * @throws DaoException the dao exception
+     */
     List<T> findAll() throws DaoException;
 
+    /**
+     * Find single entity with specified id.
+     *
+     * @param id the id
+     * @return the optional of specified entity
+     * @throws DaoException the dao exception
+     */
     Optional<T> findById(K id) throws DaoException;
 
+    /**
+     * Insert new entity
+     *
+     * @param t new entity to insert
+     * @return the boolean, which describes the success of inserting process
+     * @throws DaoException the dao exception
+     */
     boolean insert(T t) throws DaoException;
 
+    /**
+     * Update existing entity.
+     *
+     * @param t new entity to update
+     * @return the boolean, which describes the success of updating process
+     * @throws DaoException the dao exception
+     */
     boolean update(T t) throws DaoException;
 }
--- src/main/java/com/dvoraninovich/establishment/model/dao/DishDao.java
+++ src/main/java/com/dvoraninovich/establishment/model/dao/DishDao.java
@@ -6,22 +6,71 @@ import com.dvoraninovich.establishment.model.entity.Ingredient;
 
 import java.util.List;
 
-public interface DishDao extends BaseDao<Long, Dish>{
-    // TODO description
+/**
+ * {@code DishDao} interface extends {@link BaseDao} It provides functions to manipulate data of stored {@link Dish}
+ */
+public interface DishDao extends BaseDao<Long, Dish> {
+    /**
+     * Disable.
+     *
+     * @param id the dish id
+     * @return the boolean, which describes the success of dish disabling process
+     * @throws DaoException the dao exception
+     */
     boolean disable(Long id) throws DaoException;
 
-    // TODO description
+    /**
+     * Make available.
+     *
+     * @param id the dish id
+     * @return the boolean, which describes the success of making dish available  process
+     * @throws DaoException the dao exception
+     */
     boolean makeAvailable(Long id) throws DaoException;
 
-    // TODO description
+    /**
+     * Find all available.
+     *
+     * @return the list of all available dishes
+     * @throws DaoException the dao exception
+     */
     List<Dish> findAllAvailable() throws DaoException;
 
+    /**
+     * Find order dishes.
+     *
+     * @param id the order id
+     * @return the list of dishes for order with specified id
+     * @throws DaoException the dao exception
+     */
     public List<Dish> findOrderDishes(long id) throws DaoException;
 
-    // TODO description
+    /**
+     * Find dish ingredients.
+     *
+     * @param id the dish id
+     * @return the list of ingredients for specified dish
+     * @throws DaoException the dao exception
+     */
     List<Ingredient> findDishIngredients(Long id) throws DaoException;
 
+    /**
+     * Add dish ingredient.
+     *
+     * @param dishId       the dish id
+     * @param ingredientId the ingredient id
+     * @return the boolean, which describes success of adding new ingredient for specified dish
+     * @throws DaoException the dao exception
+     */
     boolean addDishIngredient(Long dishId, Long ingredientId) throws DaoException;
 
+    /**
+     * Remove dish ingredient.
+     *
+     * @param dishId       the dish id
+     * @param ingredientId the ingredient id
+     * @return the boolean, which describes success of removing ingredient for specified dish
+     * @throws DaoException the dao exception
+     */
     boolean removeDishIngredient(Long dishId, Long ingredientId) throws DaoException;
 }
